6158d2cad331d71a6f58d8a87cd31af34aa90c7b
Aryan pal <157715553+palAryan3454@users.noreply.github.com>
2024-03-21 16:14:59 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,41 +1,42 @@
 const userName = document.getElementById("name");
 const submitBtn = document.getElementById("submitBtn");
 
-const { PDFDocument, rgb } = PDFLib;
+const { PDFDocument, rgb, degrees } = PDFLib;
 
 const capitalize = (str, lower = false) =>
     (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
         match.toUpperCase()
     );
-
-submitBtn.addEventListener("click", async () => {
+submitBtn.addEventListener("click", async() => {
     const val = capitalize(userName.value);
     const isNamePresent = await checkNamePresence(val);
     if (isNamePresent) {
-        if (val.trim() !== "" && val.length <= 40 && userName.checkValidity()) {
+        if (val.trim() !== "" && userName.checkValidity()) {
             generatePDF(val);
         } else {
             userName.reportValidity();
         }
     } else {
-        alert("Please enter a valid name.");
+        alert("Don't be smart buddy. Please enter a valid name.");
     }
 });
 
-const checkNamePresence = async (name) => {
+const checkNamePresence = async(name) => {
     const nameListResponse = await fetch("./name.txt");
     const nameListText = await nameListResponse.text();
     const names = nameListText.split("\n").map((line) => line.trim().toLowerCase());
     return names.includes(name.toLowerCase());
 };
 
-const generatePDF = async (name) => {
+const generatePDF = async(name) => {
     const existingPdfBytes = await fetch("./cert.pdf").then((res) => res.arrayBuffer());
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    pdfDoc.registerFontkit(fontkit);
     const fontBytes = await fetch("./GreatVibes-Regular.ttf").then((res) => res.arrayBuffer());
     const GreatVibes = await pdfDoc.embedFont(fontBytes);
     const pages = pdfDoc.getPages();
     const firstPage = pages[0];
+
     const textWidth = GreatVibes.widthOfTextAtSize(name, 58);
     const pageWidth = firstPage.getSize().width;
     const centerX = (pageWidth - textWidth) / 2;
@@ -49,7 +50,8 @@ const generatePDF = async (name) => {
     });
 
     const pdfBytes = await pdfDoc.save();
-    var file = new File([pdfBytes], "Certificate.pdf", {
+    console.log("Done creating");
+    var file = new File([pdfBytes], "Certificate_for_Hackathon_Hustle.pdf", {
         type: "application/pdf;charset=utf-8",
     });
     saveAs(file);
